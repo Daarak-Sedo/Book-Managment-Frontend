824a82f3df53a8c3437192a12194f2b3a120a13a
RAJESH KUMAR SAHOO <rajeshkumarsahoo006@gmail.com>
2023-01-10 00:40:58 +0530

    FrontEnd

--- src/Pages/bookPage.jsx
+++ src/Pages/bookPage.jsx
@@ -1,27 +1,72 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
 
 const Books = () => {
 
+    let [BookData, setBookData] = useState([])
+
+    useEffect(() => { getStudent() }, [])
+
+    const getStudent = async () => {
+        let id = JSON.parse(localStorage.getItem("userId"))
+        console.log(id);
+        let result = await fetch(`http://localhost:3001/books`,
+            {
+                headers: {
+                    'x-api-key': JSON.parse(localStorage.getItem("token"))
+
+                }
+            })
+
+        result = await result.json()
+
+        if (result.status === false) {
+            alert(result.message)
+        } else {
+            console.log(result.data, "**");
+            setBookData(result.data)
+        }
+    }
+
+
+
+
+
+
     return (
 
         <div>
-            <h3>
-                Get All Books
-            </h3>
+            <ul>
+                <li><h3>Get All Books</h3></li>
+                <li><Link to="/" >LogOut</Link></li>
+            </ul>
 
             <div>
-                <ul>
-                    <li>Title</li>
-                    <li>Book Name</li>
-                    <li>Body</li>
+                <table>
+                    <tr>
+                        <th>title</th>
+                        <th>excerpt</th>
+                        <th>category</th>
+                        <th>reviews</th>
+                    </tr>
+                    {BookData.map((val, key) => {
+                        return (
+                            <tr key={key}>
+                                <td>{val.title}</td>
+                                <td>{val.excerpt}</td>
+                                <td>{val.category}</td>
+                                <td>{val.reviews}</td>
+                            </tr>
+                        )
+                    })}
+                </table>
 
-                </ul>
+
+
+
+
             </div>
-
-
-            <Link to="/" >LogOut</Link>
 
         </div>
     )
--- src/Pages/signup.jsx
+++ src/Pages/signup.jsx
@@ -1,9 +1,43 @@
 import React, { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import './signup.css'
 
 
 const SignUp = () => {
+
+    const navigate = useNavigate()
+
+    const [title, setTitle] = useState('')
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const SignUp = async (e) => {
+        e.preventDefault();
+        console.log(title, name, phone, email, password);
+
+        let result = await fetch('http://localhost:3001/register', {
+            method: "post",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title, name, phone, email, password })
+        });
+
+        result = await result.json()
+
+        if (result.status == false) {
+            alert(result.message)
+
+        } else {
+            localStorage.setItem("userId", JSON.stringify(result.data.userId))
+            localStorage.setItem("token", JSON.stringify(result.data.token))
+            console.log(result);
+            navigate('/login')
+        }
+    }
+
+
 
     return (
 
@@ -13,21 +47,59 @@ const SignUp = () => {
             </h3>
 
             <div className="signup-input">
-                <label>fname: </label>
-                <input type='text' placeholder="fname" /><br />
-                <label>lname: </label>
-                <input type='text' placeholder="lname" /><br />
-                <label>email: </label>
-                <input type='email' placeholder="email" /><br />
-                <label>phone: </label>
-                <input type='tel' placeholder="Phone" /><br />
-                <label>Password: </label>
-                <input type='password' placeholder="Password" /><br /><br />
+                <form>
 
-                <Link to="/login" >
-                    <button>SignUp</button>
-                </Link>
+                    <label>title: </label>
+                    <input
+                        type='text'
+                        placeholder="title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    /><br />
+                    <label>name: </label>
+                    <input
+                        type='text'
+                        placeholder="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    /><br />
+                    <label>email: </label>
+                    <input
+                        type='email'
+                        placeholder="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    /><br />
+                    <label>phone: </label>
+                    <input
+                        type='tel'
+                        placeholder="Phone"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                    /><br />
+                    <label>password: </label>
+                    <input
+                        type='password'
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    /><br /><br />
 
+                    {/* <label>address: </label>
+                <input
+                    type='text'
+                    placeholder="address"
+                /><br /><br /> */}
+
+                    {/* <Link to="/login" > */}
+                    <button className="btn" type="submit" onClick={SignUp}>SignUp</button><br />
+                    {/* </Link> */}
+
+                    <span>
+                        Already have an account ? <Link to="/login">Login.</Link>
+                    </span>
+
+                </form>
             </div>
 
 
